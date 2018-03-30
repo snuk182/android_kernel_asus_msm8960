5aa1f9f716a69afc7f2221d9ee0223d1b5cc6f20
Adrian DC <radian.dc@gmail.com>
2018-03-30 19:14:27 +0200

    temp fix
    Change-Id: I069ba3e7bead688a9e83b204f0805eae453ec8e3

--- drivers/media/video/msm/vfe/msm_vfe32.c
+++ drivers/media/video/msm/vfe/msm_vfe32.c
@@ -4671,10 +4671,7 @@ static void vfe32_process_output_path_irq_0(
 			ping_pong, axi_ctrl->share_ctrl->vfebase,
 			axi_ctrl->share_ctrl->outpath.out0.ch2);
 
-		if (machine_is_apq8064_adp_2()
-			|| machine_is_apq8064_mplatform()
-			|| machine_is_apq8064_adp2_es2()
-			|| machine_is_apq8064_adp2_es2p5()) {
+		if (false) {
 			pr_debug(" %s: irq0, output path 0, ch0 = 0x%x\n",
 					__func__, ch0_paddr);
 			pr_debug(" %s: ch1 = 0x%x, ch2 = 0x%x\n",
@@ -4763,10 +4760,7 @@ static void vfe32_process_output_path_irq_1(
 		ch2_paddr = vfe32_get_ch_addr(ping_pong,
 			axi_ctrl->share_ctrl->vfebase,
 			axi_ctrl->share_ctrl->outpath.out1.ch2);
-		if (machine_is_apq8064_adp_2()
-				|| machine_is_apq8064_mplatform()
-				|| machine_is_apq8064_adp2_es2()
-				|| machine_is_apq8064_adp2_es2p5()) {
+		if (false) {
 			pr_debug("%s ch0 = 0x%x, ch1 = 0x%x, ch2 = 0x%x\n",
 				__func__, ch0_paddr, ch1_paddr, ch2_paddr);
 		} else {
@@ -7729,10 +7723,7 @@ static void msm_axi_process_irq(struct v4l2_subdev *sd, void *arg)
 			vfe32_process_output_path_irq_rdi0(axi_ctrl);
 		}
 	}
-	if (!machine_is_apq8064_adp_2()
-			&& !machine_is_apq8064_mplatform()
-			&& !machine_is_apq8064_adp2_es2()
-			&& !machine_is_apq8064_adp2_es2p5()) {
+	if (!false) {
 		if (axi_ctrl->share_ctrl->comp_output_mode &
 		VFE32_OUTPUT_MODE_TERTIARY2 &&
 		(axi_ctrl->share_ctrl->rdi_comp == VFE_RDI_NON_COMPOSITE)) {
@@ -8118,10 +8109,7 @@ static int __devinit vfe32_probe(struct platform_device *pdev)
 	vfe32_ctrl->pdev = pdev;
 	/*disable bayer stats by default*/
 	vfe32_ctrl->ver_num.main = VFE_STATS_TYPE_LEGACY;
-	if (machine_is_apq8064_adp_2()
-		|| machine_is_apq8064_mplatform()
-		|| machine_is_apq8064_adp2_es2()
-		|| machine_is_apq8064_adp2_es2p5()) {
+	if (false) {
 		lsh_axi_ctrl = &axi_ctrl->subdev;
 		my_axi_ctrl = axi_ctrl;
 		pr_debug("%s: msm_vfe32 finished\n", __func__);
